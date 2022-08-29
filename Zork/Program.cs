049f16a531e1a350a15cdc9ec451118e35d1b476
JaeWoo Joe <joejw7554@knights.ucf.edu>
2022-08-29 11:16:56 -0400

    Zork1.2(still working)

--- Zork/Program.cs
+++ Zork/Program.cs
@@ -8,19 +8,45 @@ namespace Zork
         {
             Console.WriteLine("Welcome to Zork!");
 
-            string inputString = Console.ReadLine().Trim().ToUpper();// Application stops and waits for the user Input (blocking statement)
-            if (inputString == "QUIT")
+            string inputString = Console.ReadLine().Trim().ToUpper();
+            Commands command = ToCommand(inputString);
+            Console.WriteLine(command);
+
+        }
+
+        private static Commands ToCommand(string commandString)
+        {
+            Commands command;
+
+            if (commandString == "QUIT")
             {
-                Console.WriteLine("Thank you for playing.");
+                command = Commands.QUIT;
             }
-            else if (inputString == "LOOK")
+            else if (commandString == "LOOK")
             {
-                Console.WriteLine("This is an open field west of a white house, with a boarded front door.\nA rubber mat saying 'Welcome to Zork!' lies by the door.");
+                command = Commands.LOOK;
+            }
+            else if (commandString == "NORTH")
+            {
+                command = Commands.NORTH;
+            }
+            else if (commandString == "SOUTH")
+            {
+                command = Commands.SOUTH;
+            }
+            else if (commandString == "EAST")
+            {
+                command = Commands.EAST;
+            }
+            else if (commandString == "WEST")
+            {
+                command = Commands.WEST;
             }
             else
             {
-                Console.WriteLine($"Unknown command: {inputString}.");
+                command = Commands.UNKNOWN;
             }
+            return command;
         }
     }
 }
