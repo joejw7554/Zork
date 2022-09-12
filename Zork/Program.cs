af7f07e660b017434f70b76d3b496d3b190a2024
JaeWoo Joe <joejw7554@knights.ucf.edu>
2022-09-12 14:40:36 -0400

    Zork 2.2 (Done)

--- Zork/Program.cs
+++ Zork/Program.cs
@@ -4,6 +4,14 @@ namespace Zork
 {
     class Program
     {
+        //static string CurrentRoom
+        //{
+        //    get
+        //    {
+        //        return _rooms[_location.Row, _location.Column];
+        //    }
+        //}
+
         static void Main(string[] args)
         {
             bool isRunning = true;
@@ -11,7 +19,7 @@ namespace Zork
             Console.WriteLine("Welcome to Zork!");
             while (isRunning)
             {
-                Console.Write($"> {_rooms[_currentRoom]}\n");
+                Console.Write($"> {_rooms[_location.Row, _location.Column]}\n");
                 string inputString = Console.ReadLine().Trim();
                 Commands command = ToCommand(inputString);
 
@@ -63,25 +71,44 @@ namespace Zork
 
             switch (command)
             {
-                case Commands.NORTH:
-                case Commands.SOUTH:
-                    break;
-
-                case Commands.EAST when _currentRoom < _rooms.Length - 1:
-                    _currentRoom++;
+                case Commands.NORTH when _location.Row > _rooms.GetLength(0) - 1:
+                    _location.Row++;
                     didMove = true;
                     break;
 
-                case Commands.WEST when _currentRoom > 0:
-                    _currentRoom--;
+                case Commands.SOUTH when _location.Row > 0:
+                    _location.Row--;
+                    didMove = true;
+                    break;
+
+                case Commands.EAST when _location.Column < _rooms.GetLength(1) - 1:
+                    _location.Column++;
+                    didMove = true;
+                    break;
+
+                case Commands.WEST when _location.Column > 0:
+                    _location.Column--;
                     didMove = true;
                     break;
             }
             return didMove;
         }
-        private static readonly string[] _rooms = { "Forest", "West of House", "Behind House", "Clearing", "Canyon View" };
-        private static int _currentRoom = 1;
 
+        private static readonly string[,] _rooms = {{ "Rocky Trail", "South of House", "Canyon View"},
+                                                      { "Forest", "West of House", "Behind House"},
+                                                      {"Dense Woods", "North of House", "Clearing"} };
+        //private static int _location.Row = 1;
+        //private static int _currentLocationColumn = 1;
+
+        //private static (int Row, int Colomn) _location=(1,1);
+
+        private static Location _location = new Location() { Row = 1, Column = 1 };
+    }
+
+    internal class Location
+    {
+        public int Row;
+        public int Column;
     }
 }
 
