4f597d73194eecb5b16fc6075b0c7d957776bebb
JaeWoo Joe <joejw7554@knights.ucf.edu>
2022-11-21 15:26:34 -0500

    Change list to Que

--- Zork.Unity/Assets/Scripts/UnityOutputService.cs
+++ Zork.Unity/Assets/Scripts/UnityOutputService.cs
@@ -9,17 +9,26 @@ public class UnityOutputService : MonoBehaviour, IOutputService
     [SerializeField] TextMeshProUGUI TextLinePrefab;
     [SerializeField] Image NewLinePrefab;
     [SerializeField] Transform Content;
+    [SerializeField] int maxLines = 10;
     public void Write(object obj) => ParseAndWriteLine(obj.ToString());
     public void Write(string message) => ParseAndWriteLine(message);
     public void WriteLine(object obj) => ParseAndWriteLine(obj.ToString());
     public void WriteLine(string message) => ParseAndWriteLine(message);
-
     void ParseAndWriteLine(string message)
     {
         var textLine = Instantiate(TextLinePrefab, Content.transform);
         textLine.text = message;
-        _entries.Add(textLine.gameObject);
-    }
+        _entries.Enqueue(textLine.gameObject);
 
-    static List<GameObject> _entries = new List<GameObject>();
+        if (_entries.Count > maxLines)
+        {
+            for (int i = 0; i < maxLines; i++)
+            {
+                _entries.Dequeue();
+                //Destroy(Content.transform.GetChild(i));
+            }
+        }
+
+    }
+    static Queue<GameObject> _entries = new Queue<GameObject>();
 }
